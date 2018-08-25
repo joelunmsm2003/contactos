8c275b5ff29542995f7f11638c297c9b0afec7c7
root <root@Codigito.codigito>
2018-08-24 20:55:23 -0500

    cambios

---
+++ src/pipes/pipes.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { SubcategoriaPipe } from './subcategoria/subcategoria';
+@NgModule({
+	declarations: [SubcategoriaPipe],
+	imports: [],
+	exports: [SubcategoriaPipe]
+})
+export class PipesModule {}
